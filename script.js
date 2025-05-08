0bd30d8c8d72bb014177f8b57214cfbafb26f016
Sridhanam <dharsri255@gmail.com>
2025-05-08 11:31:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 class Animal {
-	constructor(species){
-		this._species = species;
-	}
-	get species(){
-		return this._species;
-	}
-	makeSound(){
-		console.log(`The ${this._species} makes a sound`);
-	}
-}
+    constructor(species) {
+        this._species = species; // Use a private variable
+    }
 
-class Dog extends Animal {
-	bark(){
-		console.log("woof");		
-	}
+    get species() {
+        return this._species;
+    }
+
+    makeSound() {
+        console.log(`The ${this.species} makes a sound`);
+    }
 }
 
 class Cat extends Animal {
-	purr(){
-		console.log("purr");
-	}
+    purr() {
+        console.log("purr");
+    }
 }
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+class Dog extends Animal {
+    bark() {
+        console.log("woof");
+    }
+}
+
+// Test your classes
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // Expected Output: The Siamese makes a sound
+myCat.purr(); // Expected Output: purr
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // Expected Output: The Golden Retriever makes a sound
+myDog.bark(); // Expected Output: woof
